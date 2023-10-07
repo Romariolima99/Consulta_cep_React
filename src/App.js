c6415c4cd8cbc37570d74b3a298e15c8d44ed26c
Romário Lima <devromariolima@gmail.com>
2023-10-07 20:13:23 -0300

    remoção do form, alterado para Div

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useState} from 'react'
 import './App.css';
 
 
+
 function App() {
 
 
@@ -17,14 +18,14 @@ const [erro, setErr] = useState("");
 
 const handleSearch = () => {
   if(search === "") { //verrifica se o campo esta enviando uma string vazia e não deixa o usuario enviar a requisição
-    return alert("Informe um Cep Valido")
+    return alert("Informe um Cep Valido") 
   }
   axios.get(`https://viacep.com.br/ws/${search}/json/`)
     .then((success) => {
       if(success.data.erro === true) { // verifica se o campo foi enviado como nulo || trata o retorno true enviado pela API
         return alert("Informe um Cep Valido")
       }
-      // console.log("Executando SUCCESS", success);
+      console.log("Executando SUCCESS", success);
       setLogradouro(success.data.logradouro);
       setBairro(success.data.bairro);
       setLocalidade(success.data.localidade);
@@ -52,14 +53,23 @@ const handleSearch = () => {
         <h1><b> Digite seu CEP </b></h1>
         <div className='conteudo'>
          <div className='input'>
-           <form className='form'>
-            <input type="text"  placeholder="Digite um Cep" onChange={(e) => setSearch(e.target.value)} />
-            <input type="text"  placeholder="logradouro" value={logradouro} onChange={handleSearch}/> 
-            <input type="text"  placeholder="bairro" value={bairro} onChange={handleSearch} /> 
-            <input type="text"  placeholder="localidade" value={localidade} onChange={handleSearch}/>
-            <input type="text"  placeholder="uf" value={uf} onChange={handleSearch}/> 
-            <button onClick={handleSearch}>Buscar</button>
-            </form>
+         {/* <input type="text"  placeholder="Digite um Cep" onChange={(e) => setSearch(e.target.value)} /> */}
+
+           {/* <p>{`Endereço : ${logradouro}`}</p>
+            <p>{'Complemento = '+complemento}</p>
+            { <p>{`Bairro : ${bairro}`}</p> }
+            <p>{`Cidade : ${localidade}`}</p>
+            <p>{`Unidade federativa : ${uf}`}</p> */}
+            {/* <button onClick={handleSearch}>Buscar</button>  */}
+
+            <div className='form'> 
+              <input type="text"  placeholder="Digite um Cep" onChange={(e) => setSearch(e.target.value)} />
+              <input type="text"  placeholder="logradouro" value={logradouro} /> 
+              <input type="text"  placeholder="bairro"  value={bairro} /> 
+              <input type="text"  placeholder="localidade"  value={localidade}/>
+              <input type="text"  placeholder="uf" value={uf}/> 
+              <button onClick={handleSearch}>Buscar</button>
+            </div>
           </div>
             
         </div>
