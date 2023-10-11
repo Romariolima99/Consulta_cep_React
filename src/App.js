f133eff4f883ea82a7b47608bd24336bca3cbd4c
Romário Lima <devromariolima@gmail.com>
2023-10-10 23:39:31 -0300

    Adição do toast notify

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import {useState} from 'react'
 import './App.css';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
@@ -16,15 +17,30 @@ const [uf, setUf] = useState("");
 const [erro, setErr] = useState("");
 
 
+
+
 const handleSearch = () => {
-  if(search === "") { //verrifica se o campo esta enviando uma string vazia e não deixa o usuario enviar a requisição
-    return alert("Informe um Cep Valido") 
+  if(search === "") { //verifica se o campo esta enviando uma string vazia e não deixa o usuario enviar a requisição
+    notifyError();
   }
+
+  if(search.length >= 9) { //verifica se o campo esta enviando nais de 9 digitos
+    notifyAlert();
+  }
+
+
   axios.get(`https://viacep.com.br/ws/${search}/json/`)
     .then((success) => {
-      if(success.data.erro === true) { // verifica se o campo foi enviado como nulo || trata o retorno true enviado pela API
-        return alert("Informe um Cep Valido")
+      if(success.data.erro ===  true  )  { // verifica se o campo foi enviado com um cep invalido  || trata o retorno true enviado pela API
+        notifyCepInvalido();
       }
+      // if (success.erro === 400) {
+      //   notifyStatus400();
+      // }
+      if (success.status === 200) {
+        notifySucess();
+      }
+
       console.log("Executando SUCCESS", success);
       setLogradouro(success.data.logradouro);
       setBairro(success.data.bairro);
@@ -47,7 +63,7 @@ const handleSearch = () => {
 }
 
 function limparFormulario(){
-                                                // Função para Limpar o campo
+                                              
   document.getElementById('Cep').value=''; 
   document.getElementById('logradouro').value=''; 
   document.getElementById('bairro').value=''; 
@@ -56,9 +72,36 @@ function limparFormulario(){
   document.getElementById('uf').value=''; 
 }
 
+function notifyError(){
+  toast.error("O campo não pode ser enviado vazio")
+
+}
+
+function notifyAlert(){
+  toast.warn("Numero de caracteres invalidos")
+
+}
+
+function notifyCepInvalido(){
+  toast.error("Digite um Cep Valido")
+
+}
+
+// function notifyStatus400(){
+//   toast.error("Cep Invalido")
+
+// }
+
+const notifySucess = () =>  {
+  toast.success("Consulta realizada com sucesso", {
+  className: 'custom-toast',
+
+})
+};
 
   return (
-
+    <>
+      <ToastContainer  position="top-center" />
     <div className="container">
        <div className="item">
         <h1><b> Digite seu CEP </b></h1>
@@ -66,10 +109,10 @@ function limparFormulario(){
          <div className='input'>
             <div className='form'> 
               <input type="text"  id='Cep' placeholder="Digite um Cep" onChange={(e) => setSearch(e.target.value)} />
-              <input type="text"  id='logradouro' placeholder="logradouro" value={logradouro} /> 
-              <input type="text"  id='bairro' placeholder="bairro"  value={bairro} /> 
-              <input type="text"  id='localidade' placeholder="localidade"  value={localidade}/>
-              <input type="text"  id='uf' placeholder="uf" value={uf}/> 
+              <input type="text"  id='logradouro' placeholder="Logradouro" value={logradouro} /> 
+              <input type="text"  id='bairro' placeholder="Bairro"  value={bairro} /> 
+              <input type="text"  id='localidade' placeholder="Localidade"  value={localidade}/>
+              <input type="text"  id='uf' placeholder="Uf" value={uf}/> 
               <button onClick={handleSearch}>Buscar</button>
               <button className='limpar' onClick={limparFormulario}>Limpar Dados</button>
             </div>
@@ -77,6 +120,7 @@ function limparFormulario(){
         </div>
        </div>
      </div>
+     </>
   );
 }
 
